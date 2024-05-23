9efec0a48c9b82ac43cdcd9fa7f3edca3376af0c
Giselle Arroyo <giselle.arroyor@hotmail.com>
2024-05-23 17:54:43 -0600

    purchase domain created

---
+++ src/main/java/com/spring/springcourse/domain/repository/PurchaseRepository.java
@@ -0,0 +1,13 @@
+package com.spring.springcourse.domain.repository;
+
+import com.spring.springcourse.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> gerByClient(String clientId);
+    //Optional en caso de que el cliente no tenga compas no marque error
+    Purchase save(Purchase purchase);
+}
